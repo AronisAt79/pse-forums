3f90a9bfcbf3bd90899ed1428d2f4a7f3f109240
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-12-24 20:10:29 +0200

    add logic to find the instance id

--- .github/scripts/get-instances.sh
+++ .github/scripts/get-instances.sh
@@ -15,4 +15,17 @@ get_instances() {
     echo $instances   
 }
 
-get_instances
+instances=`get_instances`
+
+# Check how many ec2 hosts match the filter criteria. There should be only one
+
+instances_length=`echo $instances | jq '. | length'
+
+if [ $instances_length -neq 1];then
+    echo "Found more than one ec2 hosts matching the filter definition. Please check the environment or filter syntax. EXITING"
+    exit 1
+fi
+
+instance_id=`jq -r .[0].ID <<< $instances`
+
+echo $instance_id
