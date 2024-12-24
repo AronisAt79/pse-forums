f6edc31b37ba6ce7024160c9a19a514f17e6db44
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-12-24 19:07:14 +0200

    create workflow script

---
+++ .github/scripts/get-instances.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+projectname=$1
+
+get_instances() {
+    instances=`aws ec2 describe-instances \
+        --filters "Name=tag:ProjectName,Values=$projectname" \
+        --query "Reservations[].Instances[].{ID:InstanceId, NAME:Tags[?Key=='Name']|[0].Value,STATE:State}" \
+        --output json `
+    
+    echo $instances   
+}
