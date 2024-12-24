685a31b5fa803fa66d8c87586cabf75f62a77c88
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-12-24 19:46:26 +0200

    add instance name variable

--- .github/scripts/get-instances.sh
+++ .github/scripts/get-instances.sh
@@ -3,10 +3,12 @@
 set -ex
 
 projectname=$1
+instancename=$2
 
 get_instances() {
     instances=`aws ec2 describe-instances \
         --filters "Name=tag:ProjectName,Values=$projectname" \
+        --filters "Name=tag:Name,Values=$instancename" \
         --query "Reservations[].Instances[].{ID:InstanceId, NAME:Tags[?Key=='Name']|[0].Value,STATE:State}" \
         --output json `
     
